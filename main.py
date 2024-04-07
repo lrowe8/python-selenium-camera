ec91cae8d5d7a6688b893f1672636f4846deeea0
lrowe8 <ssbrllc@gmail.com>
2024-04-07 10:43:16 -0600

    Video capture devices enumerating properly

--- main.py
+++ main.py
@@ -1,2 +1,67 @@
+import chromedriver_autoinstaller
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+def video_devices_webpage(path: Path) -> None:
+    with open(path, 'w') as f:
+        f.write(
+            '''
+            <!DOCTYPE html>
+
+            <html style="cursor: none;">
+            <head>
+                <script>
+                    if (!navigator.mediaDevices?.enumerateDevices) {
+                        console.log("enumerateDevices() not supported.");
+                    } else {
+                        // List cameras and microphones.
+                        navigator.mediaDevices.getUserMedia({ video: true });
+                        navigator.mediaDevices
+                            .enumerateDevices()
+                            .then((devices) => {
+                                let filteredDevices = devices.filter((device => device.kind === "videoinput"));
+
+                                filteredDevices.forEach((device) => {
+                                    console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
+                            });
+                        })
+                        .catch((err) => {
+                            console.error(`${err.name}: ${err.message}`);
+                        });
+                    }
+                </script>
+            </head>
+            <body style="background: black; display:flex; margin:0px;">
+            </body>
+            </html>
+            '''
+        )
+
 if __name__ == '__main__':
-    pass
+    cwd = Path(__file__).parent
+    file_name = "temp.html"
+
+    video_devices_webpage(cwd / file_name)
+    chrome_driver_path = chromedriver_autoinstaller.install(cwd=True)
+
+    service = Service(chrome_driver_path)
+
+    options = Options()
+    options.add_argument("--autoplay-policy=no-user-gesture-required")
+    options.add_argument("start-maximized")
+    options.add_argument("--auto-open-devtools-for-tabs")
+    options.add_argument("--use-fake-ui-for-media-stream")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(f"file://{cwd / file_name}")
+
+    input("Press Enter to exit")
+
+    try:
+        driver.close()
+    except Exception:
+        pass
